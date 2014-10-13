6091d7de87275f575b423df6cab71602cb2bf661
Jesse Miller <jmiller@jmiller.com>
2014-10-13 14:44:27 -0500

    Sometimes you only have the standard colors for prompt.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,16 +1,24 @@
 setopt PROMPT_SUBST
 
-# primary prompt
+if [[ $(echotc Co) == 256 ]]; then
+# primary prompt (256 color)
 PROMPT='[%{$FG[015]%}%D{%I:%M%p}%{$reset_color%}] %w \
 [%{$FG[117]$BG[004]%}%~%{$reset_color%}]:\
 %(!.%{$FG[001]%}.%{$FG[252]%})%{$BG[004]%}%n@%m%{$reset_color%}
 %(!.#.$) '
+else
+# primary prompt (standard color)
+PROMPT='[%{$fg_bold[white]%}%D{%I:%M%p}%{$reset_color%}] %w \
+[%{$fg_bold[cyan]$bg[blue]%}%~%{$reset_color%}]:\
+%(!.%{$fg[red]%}.%{$fg[white]%})%{$bg[blue]%}%n@%m%{$reset_color%}
+%(!.#.$) '
+fi
+
 # right
 RPROMPT='$(git_prompt_info)'
 
 # git settings
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$FG[015]$BG[004]%}branch:"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[white]$bg[blue]%}branch:"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$FG[163]%}*%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_no_bold[red]%}*%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-
