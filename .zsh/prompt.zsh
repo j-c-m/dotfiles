f8984e5e432661bcc3e0f01ca481f7897b01a394
Jesse Miller <jmiller@jmiller.com>
2014-10-13 16:48:11 -0500

    no 256 color prompt. no need.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,18 +1,10 @@
 setopt PROMPT_SUBST
 
-if [[ $(echotc Co) == 256 ]]; then
-# primary prompt (256 color)
-PROMPT='[%{%F{015}%}%D{%I:%M%p}%{%f%k%}] %w \
-[%{%F{117}%K{004}%}%~%{%f%k%}]:\
-%(!.%{%F{001}%}.%{%F{252}%})%{%K{004}%}%n@%m%{%f%k%}
-%(!.#.$) '
-else
 # primary prompt (standard color)
 PROMPT='[%{$fg_bold[white]%}%D{%I:%M%p}%{$reset_color%}] %w \
 [%{$fg_bold[cyan]$bg[blue]%}%~%{$reset_color%}]:\
 %(!.%{$fg[red]%}.%{$fg[white]%})%{$bg[blue]%}%n@%m%{$reset_color%}
 %(!.#.$) '
-fi
 
 # right
 RPROMPT='$(git_prompt_info)'
