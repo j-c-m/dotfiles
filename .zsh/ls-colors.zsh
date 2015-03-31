4e52eb86a9c67c02f7ee03d74e7a6f1e035e3724
Jesse Miller <jmiller@jmiller.com>
2015-03-31 16:21:45 -0500

    Add SunOS gls detection.

--- .zsh/ls-colors.zsh
+++ .zsh/ls-colors.zsh
@@ -1,5 +1,5 @@
 # Find the option for using colors in ls, depending on the version: Linux or BSD
-if [[ "$(uname -s)" == "NetBSD" ]]; then
+if [[ "$(uname -s)" == "NetBSD" || "$(uname -s)" == "SunOS" ]]; then
   # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
   # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
   gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
