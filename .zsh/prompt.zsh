6555dfd3887c154e5d41c5283318fb4b77c11555
Jesse Miller <jmiller@jmiller.com>
2017-04-16 23:34:35 -0500

    Standardize git zsh<->bash

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,17 +1,17 @@
 setopt PROMPT_SUBST
 
 # primary prompt (standard color)
-PROMPT='[%{$fg_bold[white]%}%D{%I:%M%p}%{$reset_color%}] %w \
-[%{$fg_bold[cyan]$bg[blue]%}%~%{$reset_color%}]:\
+PROMPT='[%{$fg_bold[white]%}%t%{$reset_color%}] %w \
+[%{$fg_bold[cyan]$bg[blue]%}%~$(git_echo_prompt)%{$reset_color%}]:\
 %(!.%{$fg[red]%}.%{$fg[white]%})%{$bg[blue]%}%n@%m%{$reset_color%}
 %(!.#.$) '
 
 # right
-RPROMPT='$(git_prompt_info)'
+RPROMPT=
 
 # git settings
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[white]$bg[blue]%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_no_bold[red]%}*%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+GIT_PROMPT_PREFIX=":"
+GIT_PROMPT_CLEAN=
+GIT_PROMPT_DIRTY="%{$fg_no_bold[red]%}*"
+GIT_PROMPT_SUFFIX=
 
