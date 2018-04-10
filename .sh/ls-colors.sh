63a8c47e912ef41b6db42bed57b2585f0275c744
Jesse Miller <jmiller@jmiller.com>
2018-04-10 14:00:24 -0500

    Better gls support on all platforms.

--- .sh/ls-colors.sh
+++ .sh/ls-colors.sh
@@ -1,19 +1,21 @@
 if type -p dircolors &> /dev/null;then
     eval $(dircolors ~/.sh/dircolors.256dark)
+elif type -p gdircolors &> /dev/null;then
+    eval $(gdircolors ~/.sh/dircolors.256dark)
 fi
 
 export LSCOLORS=Exfxcxdxbxegedabagacad
 
 if [[ -n "$LS_COLORS" ]]; then
 	if type -p gls &> /dev/null; then
-		if type -p colorls &> /dev/null; then
-			colorls -G -d . &>/dev/null && alias ls='colorls -G'
-		else
-			gls --color -d . &>/dev/null && alias ls='gls --color=tty'
-		fi
+		gls --color -d . &>/dev/null && alias ls='gls --color=tty'
 	else
 		ls --color -d . &>/dev/null && alias ls='ls --color=tty'
 	fi
 else
-	ls -G . &>/dev/null && alias ls='ls -G'
+	if type -p colorls &> /dev/null; then
+		colorls -G -d . &>/dev/null && alias ls='colorls -G'
+	else
+		ls -G . &>/dev/null && alias ls='ls -G'
+	fi
 fi
