4d8c751386ab0bae2c146495b9411ea2bc281543
Jesse Miller <jmiller@jmiller.com>
2018-08-24 01:26:37 -0500

    Add upgrade function

---
+++ .sh/upgrade.sh
@@ -0,0 +1,18 @@
+function upgrade_dotfiles() {
+    local OPWD
+    OPWD=$PWD
+
+    cd $HOME
+    if type -p curl &> /dev/null; then
+        curl -L https://github.com/j-c-m/dotfiles/tarball/master | \
+            tar -xvzf - --no-same-owner  --no-same-permissions \
+            --strip-components 1 --exclude={README.md,LICENSE}
+    elif type -p wget &> /dev/null; then
+        wget https://github.com/j-c-m/dotfiles/tarball/master -O - | \
+            tar -xvzf - --no-same-owner  --no-same-permissions \
+            --strip-components 1 --exclude={README.md,LICENSE}
+    else
+        echo "curl or wget not found."
+    fi
+    cd $OPWD
+}
