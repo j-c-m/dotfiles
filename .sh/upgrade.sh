1eb81f8131625c6a3d3297720f88d77816efaad5
Jesse Miller <jmiller@jmiller.com>
2019-02-11 01:06:41 -0600

    GNU tar detection

--- .sh/upgrade.sh
+++ .sh/upgrade.sh
@@ -2,14 +2,20 @@ function upgrade_dotfiles() {
     local OPWD
     OPWD=$PWD
 
+    local TAR
+    TAR=tar
+    if type -p gtar &> /dev/null; then
+        TAR=gtar
+    fi
+
     cd $HOME
     if type -p curl &> /dev/null; then
-        curl -L https://github.com/j-c-m/dotfiles/tarball/master | \
-            tar -xvzf - --no-same-owner  --no-same-permissions \
+        curl -k -L https://github.com/j-c-m/dotfiles/tarball/master | \
+            $TAR -xvzf - --no-same-owner  --no-same-permissions \
             --strip-components 1 --exclude={README.md,LICENSE}
     elif type -p wget &> /dev/null; then
         wget https://github.com/j-c-m/dotfiles/tarball/master -O - | \
-            tar -xvzf - --no-same-owner  --no-same-permissions \
+            $TAR -xvzf - --no-same-owner  --no-same-permissions \
             --strip-components 1 --exclude={README.md,LICENSE}
     else
         echo "curl or wget not found."
