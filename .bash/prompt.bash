7095d70c316060bb27e2f2556467fcae377d810f
Jesse Miller <jmiller@jmiller.com>
2019-02-15 22:48:56 -0600

    Sync bash prompt.
    red root
    24H time

--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -7,7 +7,13 @@ case $TERM in
         ;;
 esac
 
-PS1="${TITLEBAR}[\[\e[1;37m\]\T\\[\e[m\]] \d [\[\e[44m\e[1;36m\]\w\$(git_echo_prompt)\[\e[m\]]:\[\e[44m\e[37m\]\u@\h\[\e[m\]\n\$ "
+if [[ $EUID -eq 0 ]]; then
+    UCOLOR="\e[31m"
+else
+    UCOLOR="\e[37m"
+fi
+
+PS1="${TITLEBAR}[\[\e[1;37m\]\t\\[\e[m\]] \d [\[\e[44m\e[1;36m\]\w\$(git_echo_prompt)\[\e[m\]]:\[\e[44m${UCOLOR}\]\u@\h\[\e[m\]\n\$ "
 
 GIT_PROMPT_PREFIX=":"
 GIT_PROMPT_SUFFIX=
