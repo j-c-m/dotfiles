d828f3a5ae073e5883622f68c5dc6e0d6154a8d7
Jesse Miller <jmiller@jmiller.com>
2019-12-23 13:48:37 -0600

    Fix default check.

--- .sh/base16.sh
+++ .sh/base16.sh
@@ -2,7 +2,8 @@
 BASE16_SHELL="$HOME/.config/base16-shell"
 
 if [ -n "$PS1" -a -s "$BASE16_SHELL/profile_helper.sh" ]; then
-	[ ! -a "$HOME/.base16_theme" ] && \
+	[ ! -e "$HOME/.base16_theme" ] && \
 		ln -s "$BASE16_SHELL/scripts/base16-eighties.sh" "$HOME/.base16_theme"
     eval "$("$BASE16_SHELL/profile_helper.sh")"
 fi
+
