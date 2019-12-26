0a8a0aba39bfe626f29804fd6fa7b07c01ba9a14
Jesse Miller <jmiller@jmiller.com>
2019-12-26 11:57:15 -0600

    Cache base16 profile / Early umask.

--- .sh/base16.sh
+++ .sh/base16.sh
@@ -4,6 +4,7 @@ BASE16_SHELL="$HOME/.config/base16-shell"
 if [ -n "$PS1" -a -s "$BASE16_SHELL/profile_helper.sh" ]; then
 	[ ! -e "$HOME/.base16_theme" ] && \
 		ln -s "$BASE16_SHELL/scripts/base16-eighties.sh" "$HOME/.base16_theme"
-    eval "$("$BASE16_SHELL/profile_helper.sh")"
+    [ ! -f "$HOME/.base16_profile_cache.sh" ] && \
+        "$BASE16_SHELL/profile_helper.sh" > "$HOME/.base16_profile_cache.sh"
+    source "$HOME/.base16_profile_cache.sh"
 fi
-
--- .sh/post_upgrade.sh
+++ .sh/post_upgrade.sh
@@ -1,3 +1,4 @@
 # _go rename _golang
 rm -f $HOME/.zsh/compdef/_go
 rm -f $HOME/.sh/xterm-colors.sh
+rm -f $HOME/.sh/umask.sh
