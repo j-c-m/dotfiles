4ee1181fe57b097b1a5c79887aac7bce94b45106
Jesse Miller <jmiller@jmiller.com>
2025-08-10 14:41:08 -0600

    working on auto-upgrade

--- .sh/upgrade.sh
+++ .sh/upgrade.sh
@@ -1,3 +1,26 @@
+VERSION=0.0.1
+
+check_and_upgrade_dotfiles() {
+    # Define local version (you should set this to your current version)
+    local LOCAL_VERSION="$VERSION"  # Replace with your actual local version
+
+    # Fetch the remote upgrade.sh file and extract the VERSION line
+    REMOTE_VERSION=$(curl -s https://raw.githubusercontent.com/j-c-m/dotfiles/refs/heads/master/.sh/upgrade.sh | head -n 1 | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+
+    if [ -z "$REMOTE_VERSION" ]; then
+        echo "Error: Could not retrieve or parse remote version."
+        return 1
+    fi
+
+    # Compare versions
+    if [ "$(printf '%s\n%s' "$REMOTE_VERSION" "$LOCAL_VERSION" | sort -V | tail -n 1)" = "$REMOTE_VERSION" ] && [ "$REMOTE_VERSION" != "$LOCAL_VERSION" ]; then
+        echo "New version $REMOTE_VERSION available (local version: $LOCAL_VERSION). Upgrading..."
+        upgrade_dotfiles
+    else
+        echo "No upgrade needed. Local version $LOCAL_VERSION is up to date."
+    fi
+}
+
 function upgrade_dotfiles() {
     local OPWD
     OPWD=$PWD
