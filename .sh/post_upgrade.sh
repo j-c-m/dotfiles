0be77b784d091d7e37f0b98dbb5d43140a55b649
Jesse Miller <jmiller@jmiller.com>
2025-08-13 15:11:20 -0600

    More base16 removal

--- .sh/post_upgrade.sh
+++ .sh/post_upgrade.sh
@@ -4,5 +4,6 @@ rm -f $HOME/.sh/xterm-colors.sh
 rm -f $HOME/.sh/umask.sh
 rm -f $HOME/.sh/base16.sh
 rm -f $HOME/.zsh/00-spectrum.zsh
-rm -f $HOME/.config/base16-shell
+rm -f $HOME/.sh/05-base16.sh
+rm -rf $HOME/.config/base16-shell
 [ -L "$HOME/.base16_theme" ] && rm "$HOME/.base16_theme"
