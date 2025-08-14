402961a6ea1aa11f832b4ac477f845348f34357a
Jesse Miller <jmiller@jmiller.com>
2025-08-14 15:45:32 -0600

    Restore alacritty.sh

--- .sh/post_upgrade.sh
+++ .sh/post_upgrade.sh
@@ -5,6 +5,5 @@ rm -f $HOME/.sh/umask.sh
 rm -f $HOME/.sh/base16.sh
 rm -f $HOME/.zsh/00-spectrum.zsh
 rm -f $HOME/.sh/05-base16.sh
-rm -f $HOME/.sh/01-alacritty.sh
 rm -rf $HOME/.config/base16-shell
 [ -L "$HOME/.base16_theme" ] && rm "$HOME/.base16_theme"
