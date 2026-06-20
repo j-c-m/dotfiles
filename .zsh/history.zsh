150aa8f619e0421f0bc3b76de8f3e318b44eb18e
Jesse Miller <jmiller@jmiller.com>
2026-06-20 11:07:46 -0600

    zsh: improve history dedup and increase size to 100k
    Add hist_find_no_dups and hist_reduce_blanks for cleaner history
    browsing and storage. Bump HISTSIZE and SAVEHIST from 10k to 100k.

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,11 +1,13 @@
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 HISTFILE=~/.zsh_history
 
 setopt extended_history
 setopt hist_expire_dups_first
+setopt hist_find_no_dups
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
+setopt hist_reduce_blanks
 setopt hist_verify
 setopt inc_append_history
 setopt share_history # share command history data
