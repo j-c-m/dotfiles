b513e2dcb4befcc6c28421b1a214a26ed8f737b6
Jesse Miller <jmiller@jmiller.com>
2026-09-01 17:57:00 -0600

    Add nvim LazyVim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    main = "black-metal",
+    opts = {
+      theme = "bathory",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    opts = {
+      theme = "dragon",
+      background = {
+        dark = "dragon",
+        light = "lotus",
+      },
+      colors = {
+        palette = {
+          dragonBlack0 = "#000000",
+          dragonBlack1 = "#000000",
+          dragonBlack2 = "#000000",
+          dragonBlack3 = "#000000",
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eighties-black",
+    },
+  },
+}
